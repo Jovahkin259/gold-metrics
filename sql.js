e33a15427a0e6501bd8356a59d4cb336c67d7dc5
Joshua Astle <jovahkin259@gmail.com>
2021-03-14 08:36:57 +0000

    Initial Commit

---
+++ sql.js
@@ -0,0 +1,133 @@
+const sqlite3 = require('sqlite3')
+const db = new sqlite3.Database('./gold_medals.sqlite')
+
+/*
+Returns a SQL query string that will create the Country table with four columns: name (required), code (required), gdp, and population.
+*/
+const createGoldMedalTable = () => {}
+/*
+Returns a SQL query string that will create the GoldMedal table with ten columns (all required): id, year, city, season, name, country, gender, sport, discipline, and event.
+*/
+const createCountryTable = () => {}
+/*
+Returns a SQL query string that will find the number of gold medals for the given country.
+*/
+
+const goldMedalNumber = country => {
+
+}
+
+/*
+Returns a SQL query string that will find the year where the given country
+won the most summer medals, along with the number of medals aliased to 'count'.
+*/
+
+const mostSummerWins = country => {
+
+}
+
+/*
+Returns a SQL query string that will find the year where the given country
+won the most winter medals, along with the number of medals aliased to 'count'.
+*/
+
+const mostWinterWins = country => {
+
+}
+
+/*
+Returns a SQL query string that will find the year where the given country
+won the most medals, along with the number of medals aliased to 'count'.
+*/
+
+const bestYear = country => {
+
+}
+
+/*
+Returns a SQL query string that will find the discipline this country has
+won the most medals, along with the number of medals aliased to 'count'.
+*/
+
+const bestDiscipline = country => {
+
+}
+
+/*
+Returns a SQL query string that will find the sport this country has
+won the most medals, along with the number of medals aliased to 'count'.
+*/
+
+const bestSport = country => {
+
+}
+
+/*
+Returns a SQL query string that will find the event this country has
+won the most medals, along with the number of medals aliased to 'count'.
+*/
+
+const bestEvent = country => {
+
+}
+
+/*
+Returns a SQL query string that will find the number of male medalists.
+*/
+
+const numberMenMedalists = country => {
+
+}
+
+/*
+Returns a SQL query string that will find the number of female medalists.
+*/
+
+const numberWomenMedalists = country => {
+
+}
+
+/*
+Returns a SQL query string that will find the athlete with the most medals.
+*/
+
+const mostMedaledAthlete = country => {
+
+}
+
+/*
+Returns a SQL query string that will find the medals a country has won
+optionally ordered by the given field in the specified direction.
+*/
+
+const orderedMedals = (country, field, sortAscending) => {
+
+}
+
+/*
+Returns a SQL query string that will find the sports a country has
+won medals in. It should include the number of medals, aliased as 'count',
+as well as the percentage of this country's wins the sport represents,
+aliased as 'percent'. Optionally ordered by the given field in the specified direction.
+*/
+
+const orderedSports = (country, field, sortAscending) => {
+
+}
+
+module.exports = {
+  createCountryTable,
+  createGoldMedalTable,
+  goldMedalNumber,
+  mostSummerWins,
+  mostWinterWins,
+  bestDiscipline,
+  bestSport,
+  bestYear,
+  bestEvent,
+  numberMenMedalists,
+  numberWomenMedalists,
+  mostMedaledAthlete,
+  orderedMedals,
+  orderedSports
+}
